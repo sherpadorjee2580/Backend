76a8e2e4250ed5d53ca602bca37990fa299d54dc
Dorji <sherpadorjee2580@gmail.com>
2026-02-12 14:19:43 +0545

    learning express dependencies

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-// const express = require("express");
-// const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
-// const app = express();
-// app.use(cors)
+const app = express();
 
-// app.get("/", (req, res) => {
-//   console.log("Hello world");
-// });
+app.use(cors());
+app.use(express.json());
 
-// app.listen(5000, () => console.log("App is running in port 5000"));
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  //   res.json({
+  //     Message: "Hello",
+  //     status: "running",
+  //   });
+  //   res.write("Hello");
+  res.send("My name is Dorji Sherpa");
+});
+
+app.listen(PORT, () => {
+  console.log("App is running on port 5000");
+});
