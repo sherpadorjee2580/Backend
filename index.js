071b66860f6ba9f1048e3bae8b9bf43a7360157d
Dorji <sherpadorjee2580@gmail.com>
2026-02-12 13:25:36 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+// const cors = require("cors")
+
+// const app = express();
+// app.use(cors)
+
+// app.get("/", (req, res) => {
+//   console.log("Hello world");
+// });
+
+// app.listen(5000, () => console.log("App is running in port 5000"));
---
+++ model/userMode.js
@@ -0,0 +1,26 @@
+const moongose = require("mongoose");
+
+const userSchema = mongoose.Scheme({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Usesr", userSchema);
